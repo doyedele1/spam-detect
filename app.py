32c1cb46dba00635abd704bb5e665f00bb16f316
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-05-10 04:37:18 +0100

    separating frontend from backend

--- app.py
+++ app.py
@@ -1,40 +1,71 @@
 # Web Packages
-from flask import Flask, render_template, jsonify, request
-
-# ML Packages
-import os
+from flask import Flask, request, jsonify
+from sklearn.externals import joblib
+import traceback
+import numpy as np
 import pickle
-from sklearn import *
-from collections import Counter
-
+import pandas as pd
 from model import make_dict
 
 app = Flask(__name__)
 
-# Saved machine learning model
-clf = pickle.load(open("text-classifier.mdl", "rb"))
-
 @app.route('/')
 def home():
-    return render_template('index.html')
 
-@app.route("/about")
-def about():
-  return render_template("about.html")
+    return "Hello World!"
 
-@app.route('/predict', methods=['POST'])
+
+@app.route('/api', methods=['POST'])
 def predict():
-    # For rendering predicted result on the GUI
-    d = make_dict()
 
-    # if request.method == 'POST':
-    inp = request.form['inp']
-    # data_in = [[email]]
-    features = []
-    for word in d:
-        features.append(inp.count(word[0]))
-    res = clf.predict([features])
-    return render_template('index.html', prediction = res)
+    if clf:
+        try:
 
-if __name__== '__main__':
-    app.run(debug=True)
+            data = request.get_json()
+
+            email_input = data['email']
+
+            d = make_dict()
+
+            features = []
+            
+            # inp = raw_input(">").split()
+
+            for word in d:
+                features.append(email_input.count(word[0]))
+            
+            prediction = clf.predict([features])
+
+            if prediction == [0]:
+                return "Not Spam"
+            return "Spam"
+
+            # print(type(prediction))
+
+            return jsonify({'prediction': str(prediction)})
+            # print res # Not Spam - [0], Spam - [1]
+            # print(["Not Spam", "Spam!"][res[0]])
+            # print(model.accuracy_score)
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+
+    else:
+        print ('Train the model first')
+        return ('No model here to use')
+    
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1]) # This is for a command-line input
+
+    except:
+        port = 12345 # If you don't provide any port the port will be set to 12345
+
+
+    # Load machine learning model
+    clf = pickle.load(open("text-classifier.mdl", "rb"))
+    print('Model loaded')
+
+    app.run(port=port, debug=True)
--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import os
 from collections import Counter
+from sklearn import *
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split as tts
 from sklearn.metrics import accuracy_score
@@ -29,47 +30,59 @@ def make_dict():
     return dictionary.most_common(3000)
 
 # Prepare the dataset using feature vectorization
-def make_dataset(dictionary):
-    direc = "emails/"
-    files = os.listdir(direc)
+# def make_dataset(dictionary):
+#     direc = "emails/"
+#     files = os.listdir(direc)
     
-    emails = [direc + email for email in files]
-    feature_set = []
-    labels = []
-    pickle = len(emails)
+#     emails = [direc + email for email in files]
+#     feature_set = []
+#     labels = []
+#     pickle = len(emails)
 
-    for email in emails:
-        data = []
-        f = open(email)
-        words = f.read().split(' ')
-        for entry in dictionary:
-            data.append(words.count(entry[0]))
-        feature_set.append(data)
+#     for email in emails:
+#         data = []
+#         f = open(email)
+#         words = f.read().split(' ')
+#         for entry in dictionary:
+#             data.append(words.count(entry[0]))
+#         feature_set.append(data)
         
-        if "ham" in email:
-            labels.append(0)
-        if "spam" in email:
-            labels.append(1)
-        pickle = pickle - 1
-    return feature_set, labels
+#         if "ham" in email:
+#             labels.append(0)
+#         if "spam" in email:
+#             labels.append(1)
+#         pickle = pickle - 1
+#     return feature_set, labels
 
 
 d = make_dict()
-features, labels = make_dataset(d)
+# features, labels = make_dataset(d)
 
 # Percentages of training and test data
-x_train, x_test, y_train, y_test = tts(features, labels, test_size=0.2)
+# x_train, x_test, y_train, y_test = tts(features, labels, test_size=0.2)
 
 # Train the model using a built-in classifier
-clf = MultinomialNB()
-clf.fit(x_train, y_train)
+# clf = MultinomialNB()
+# clf.fit(x_train, y_train)
 
-preds = clf.predict(x_test)
-# print accuracy_score(y_test, preds)
+# preds = clf.predict(x_test)
+# print ("Probability: %.2f" % accuracy_score(y_test, preds))
 
-# Save the model to disk
-filename = 'text-classifier.mdl'
-pickle.dump(clf, open(filename, 'wb'))
+# # Save the model to disk
+# filename = 'text-classifier.mdl'
+# pickle.dump(clf, open(filename, 'wb'))
 
-# Load the model from disk
-clf = pickle.load(open(filename, 'rb'))
+
+# Load machine learning model
+# clf = pickle.load(open("text-classifier.mdl", "rb"))
+# print('Model loaded')
+
+
+# features = []
+# inp = raw_input(">").split()
+# for word in d:
+#     features.append(inp.count(word[0]))
+# res = clf.predict([features])
+# # print res # Not Spam - [0], Spam - [1]
+# print(["Not Spam", "Spam!"][res[0]])
+# print(model.accuracy_score)
